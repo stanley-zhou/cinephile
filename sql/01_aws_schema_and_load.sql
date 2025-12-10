2bc5e7e0a2cb93c7ce478ba939d1d1c90e90ca87
stanleyzhou <stanleyzhou.0620@mail.com>
2025-12-10 02:44:37 -0500

    new data cleaning, shcema normalization, DDL

--- sql/01_aws_schema_and_load.sql
+++ sql/01_aws_schema_and_load.sql
@@ -4,6 +4,9 @@ DROP TABLE IF EXISTS movie_genres;
 DROP TABLE IF EXISTS people;
 DROP TABLE IF EXISTS genres;
 DROP TABLE IF EXISTS movies;
+DROP TABLE IF EXISTS movie_production_countries  CASCADE;
+DROP TABLE IF EXISTS countries                 CASCADE;
+DROP TABLE IF EXISTS market_info                CASCADE;
 
 CREATE TABLE movies (
                         movie_id        VARCHAR(20) PRIMARY KEY,
@@ -52,3 +55,26 @@ CREATE TABLE person_known_for (
                                   FOREIGN KEY (movie_id) REFERENCES movies(movie_id)
 );
 
+CREATE TABLE market_info (
+                            tmdb_id      INTEGER       PRIMARY KEY,
+                            imdb_id      VARCHAR(20)   NOT NULL,
+                            budget       NUMERIC(15,2),
+                            revenue      NUMERIC(15,2),
+                            popularity   NUMERIC(10,2),
+                            overview     TEXT,
+                            poster_path  TEXT,
+                            FOREIGN KEY (imdb_id) REFERENCES movies(movie_id)
+);
+
+CREATE TABLE countries (
+                           country_name  TEXT PRIMARY KEY
+);
+
+
+CREATE TABLE movie_production_countries (
+                                          tmdb_id      INTEGER  NOT NULL,
+                                          country_name TEXT     NOT NULL,
+                                          PRIMARY KEY (tmdb_id, country_name),
+                                          FOREIGN KEY (tmdb_id) REFERENCES market_info(tmdb_id),
+                                          FOREIGN KEY (country_name) REFERENCES countries(country_name)
+);
